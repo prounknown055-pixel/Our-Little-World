1a08984b1037c4ad8278cb719b3543b7e1b44a38
prounknown055-pixel <prounknown055@gmail.com>
2026-07-07 01:20:51 +0530

    Update scene.js

--- scripts/scene.js
+++ scripts/scene.js
@@ -1,1 +1,50 @@
+export const scene = new THREE.Scene();
 
+scene.background = new THREE.Color(0x87CEEB);
+
+scene.fog = new THREE.Fog(0x87CEEB,80,300);
+
+export const renderer = new THREE.WebGLRenderer({
+    canvas:document.getElementById("game"),
+    antialias:true
+});
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+
+renderer.shadowMap.enabled=true;
+
+const ambient=new THREE.AmbientLight(0xffffff,1.8);
+
+scene.add(ambient);
+
+const sun=new THREE.DirectionalLight(0xffffff,2);
+
+sun.position.set(50,80,50);
+
+sun.castShadow=true;
+
+scene.add(sun);
+
+const ground=new THREE.Mesh(
+
+new THREE.PlaneGeometry(500,500),
+
+new THREE.MeshStandardMaterial({
+color:0x4CAF50
+})
+
+);
+
+ground.rotation.x=-Math.PI/2;
+
+ground.receiveShadow=true;
+
+scene.add(ground);
+
+window.addEventListener("resize",()=>{
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+});
